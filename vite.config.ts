21676b34d86f9a78c19efee74cfcec2d32bf9e22
MatDoney <matthis.doney@gmail.com>
2025-02-06 12:00:07 +0100

    pwa mise en place

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,32 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-      VitePWA({
-        registerType: 'autoUpdate',
-      })
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'], // Ajout de jpeg et webp
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => { // Utilisation d'une fonction pour plus de flexibilité
+              return true; // Cache tout par défaut
+              // Ou une condition plus précise:
+              // return request.destination === 'image' || request.destination === 'script' || request.destination === 'style' || request.destination === 'document';
+            },
+            handler: 'NetworkFirst', // Stratégie de cache : d'abord le réseau, puis le cache
+            options: {
+              cacheName: 'leitner-cache',
+              expiration: {
+                maxAgeSeconds: 24 * 3600, // 24 heures
+                maxEntries: 30 // Optionnel : nombre maximal d'entrées dans le cache
+              },
+              cacheableResponse: {
+                statuses: [0, 200], // Cache les réponses avec les status 0 (pour les fichiers locaux) et 200 (OK)
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
